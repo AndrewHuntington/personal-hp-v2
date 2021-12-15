1565e360d8f65a7340eccaa46802ebbb0fea14ba
Andrew Huntington <andrew.huntington@gmail.com>
2021-12-15 12:28:33 +0900

    Fix linter to play nice with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
-const App = () => {
-    return <h1 className="text-3xl font-bold underline"> Hello world! </h1>;
-};
+function App() {
+    return (
+        <>
+            <Navbar />
+            <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+        </>
+    );
+}
 
 export default App;
